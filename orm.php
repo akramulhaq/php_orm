e9b46d073f05e60f7a22d3ae04b6857bc4b9f20a
Ekramul Hoque <akramul.haq5@gmail.com>
2017-11-21 17:58:01 +0600

    all internal variable with underscore

--- orm.php
+++ orm.php
@@ -2,111 +2,110 @@
 
 
 class Orm extends mysqli {
-    private $table   = "users";
-    private $sql     = "";
-    public  $host    = "localhost";
-    public  $username= "root";
-    public  $password= "hello";
-    public  $db      = "lsapp";
-    public  $result;
-    public  $fields=array();
-    public  $field_list=array();
-    public  $error   = "";
+    private $_table   = "users";
+    private $_sql     = "";
+    public  $_host    = "localhost";
+    public  $_username= "root";
+    public  $_password= "hello";
+    public  $_db      = "lsapp";
+    public  $_result;
+    public  $_fields=array();
+    public  $_field_list=array();
+    public  $_error   = "";
     
     public function __construct(){
-        parent::__construct($this->host,$this->username,$this->password,$this->db);
+        parent::__construct($this->_host,$this->_username,$this->_password,$this->_db);
         $this->init();
     }
     
     public function init(){
         if($this->columns()->get()->result()){
-            while($column   = $this->result->fetch_assoc())
+            while($column   = $this->_result->fetch_assoc())
             {    
                 
                 $column_name        = $column['column_name'];
                 unset($column['column_name']);
                 $column['value']    = '';
-                $this->fields[$column_name] = $column;
-                $this->field_list[] = $column_name;
+                $this->_fields[$column_name] = $column;
+                $this->_field_list[] = $column_name;
             }
         }
-        //$this->fields = $this->columns()->get()->result()->fetch_all(MYSQLI_ASSOC);
+        
         return $this;
     }
     
     public function change_db($database){
-        $this->db = $database;
+        $this->_db = $database;
         $this->select_db($database);
     }
     
     public function __set($name, $value)
     {
-        if(in_array($name,$this->field_list)){
+        if(in_array($name,$this->_field_list)){
             $this->$name = $value;
-            $this->fields[$name]['value'] = $value;
+            $this->_fields[$name]['value'] = $value;
         }else{
-            $this->error = "Column not found";
+            $this->_error = "Column not found";
             return false;
         }
     }
     
-    public function get(){
-        if(!$this->sql){
+    public function get($id=''){
+        if($id){
+            $this->_sql = "SELECT * FROM {$this->_table} where id='$id'";
+        }
+        if(!$this->_sql){
             $this->get_all();
         }
-        $this->query($this->sql);
+        $this->query($this->_sql);
         return $this;
     }
     
     public function get_all(){
-        $this->sql = "SELECT * FROM {$this->table}";
+        $this->_sql = "SELECT * FROM {$this->_table}";
         return $this;
     }
     
     public function query($sql='',$resultmode = MYSQLI_USE_RESULT){
         if(!$sql)
-            $sql = $this->sql;
-        $this->result = parent::query($sql,$resultmode);
-        if(!$this->result){
-            die("Error Occured : ".$this->error);
+            $sql = $this->_sql;
+        $this->_result = parent::query($this->_sql,$resultmode);
+        if(!$this->_result){
+            die("Error Occured : {$this->_sql} ".$this->error);
         }
         return $this;
     }
     
     public function result(){
-        return $this->result;
+        return $this->_result;
     }
     
     
     public function save(){
-       $insert_sql = "INSERT INTO {$this->table} (`".implode('`,`',$this->field_list)."`) VALUES (" ;
-       $this->sql  = $insert_sql;
-       foreach($this->fields as $field){
-           $this->sql .= "'{$field['value']}',";
+       $insert_sql = "INSERT INTO {$this->_table} (`".implode('`,`',$this->_field_list)."`) VALUES (" ;
+       $this->_sql  = $insert_sql;
+       foreach($this->_fields as $field){
+           $this->_sql .= "'{$field['value']}',";
        }
-       $this->sql  = rtrim($this->sql,',');
-       $this->sql .= ")";
-       $this->query($this->sql);
+       $this->_sql  = rtrim($this->_sql,',');
+       $this->_sql .= ")";
+       $this->query($this->_sql);
        return $this; 
     }
     
     public function columns(){
-        $this->sql = "SELECT  
+        $this->_sql = "SELECT  
                             column_name,
                             data_type,
                             column_key,
                             character_maximum_length length,
                             numeric_precision  int_length
                         FROM `INFORMATION_SCHEMA`.`COLUMNS` 
-                        WHERE `TABLE_SCHEMA`='{$this->db}' 
-                            AND `TABLE_NAME`='{$this->table}'";
+                        WHERE `table_SCHEMA`='{$this->_db}' 
+                            AND `table_NAME`='{$this->_table}'";
         
         return $this;
     }
 }
 
-$orm = new Orm();
-
-$orm->name  = "Md Ekramul Hoque12";
-$orm->email = "akramul.haq5@gmail.com";
-$orm->save();
+ 
