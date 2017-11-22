3a7de5dad1621585fc75e7d863aca022cd76c855
Ekramul Hoque <akramul.haq5@gmail.com>
2017-11-22 17:14:36 +0600

    change db to seperate property

--- orm.php
+++ orm.php
@@ -1,23 +1,30 @@
 <?php
 
 
-class Orm extends mysqli {
+class Orm  {
     private $_table   = "users";
     private $_sql     = "";
     public  $_host    = "localhost";
     public  $_username= "root";
     public  $_password= "hello";
-    public  $_db      = "lsapp";
+    public  $_db      ;
+    public  $_db_name = "lsapp";
     public  $_result;
     public  $_fields=array();
     public  $_field_list=array();
     public  $_error   = "";
     
     public function __construct(){
-        parent::__construct($this->_host,$this->_username,$this->_password,$this->_db);
+        
+        $this->_db = new mysqli($this->_host,$this->_username,$this->_password,$this->_db_name);
         $this->init();
     }
-    
+    public function db(){
+        return $this->_db;
+    }
+    public function sql(){
+        return $this->_sql;
+    }
     public function init(){
         if($this->columns()->get()->result()){
             while($column   = $this->_result->fetch_assoc())
@@ -35,8 +42,8 @@ class Orm extends mysqli {
     }
     
     public function change_db($database){
-        $this->_db = $database;
-        $this->select_db($database);
+        $this->_db_name = $database;
+        $this->_db->select_db($database);
     }
     
     public function __set($name, $value)
@@ -55,13 +62,13 @@ class Orm extends mysqli {
             $this->_sql = "SELECT * FROM {$this->_table} where id='$id'";
         }
         if(!$this->_sql){
-            $this->get_all();
+            $this->all();
         }
         $this->query($this->_sql);
         return $this;
     }
     
-    public function get_all(){
+    public function all(){
         $this->_sql = "SELECT * FROM {$this->_table}";
         return $this;
     }
@@ -69,7 +76,7 @@ class Orm extends mysqli {
     public function query($sql='',$resultmode = MYSQLI_USE_RESULT){
         if(!$sql)
             $sql = $this->_sql;
-        $this->_result = parent::query($this->_sql,$resultmode);
+        $this->_result = $this->_db->query($this->_sql,$resultmode);
         if(!$this->_result){
             die("Error Occured : {$this->_sql} ".$this->error);
         }
@@ -80,6 +87,9 @@ class Orm extends mysqli {
         return $this->_result;
     }
     
+    public function data(){
+        return $this->_result->fetch_all();
+    }
     
     public function save(){
        $insert_sql = "INSERT INTO {$this->_table} (`".implode('`,`',$this->_field_list)."`) VALUES (" ;
@@ -101,11 +111,15 @@ class Orm extends mysqli {
                             character_maximum_length length,
                             numeric_precision  int_length
                         FROM `INFORMATION_SCHEMA`.`COLUMNS` 
-                        WHERE `table_SCHEMA`='{$this->_db}' 
+                        WHERE `table_SCHEMA`='{$this->_db_name}' 
                             AND `table_NAME`='{$this->_table}'";
         
         return $this;
     }
 }
+
+$orm = new Orm();
+$orm->get_all()->get();
+var_dump($orm->result()->fetch_all());
 
  
