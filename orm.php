ec3011d455cd945dc3639af620d1c70696ba5201
Ekramul Hoque <akramul.haq5@gmail.com>
2017-11-28 11:52:12 +0600

    add where clause function

--- orm.php
+++ orm.php
@@ -2,19 +2,21 @@
 
 
 class Orm  {
-    private $_table   = "users";
-    private $_id      = "";
-    private $_sql     = "";
-    private $_last_sql     = "";
-    public  $_host    = "localhost";
-    public  $_username= "root";
-    public  $_password= "hello";
-    public  $_db      ;
-    public  $_db_name = "lsapp";
+    public $_table     = "users";
+    private $_id        = "";
+    private $_sql       = "";
+    private $_where_sql = "";
+    private $_last_sql  = "";
+    public  $_host      = "localhost";
+    public  $_username  = "root";
+    public  $_password  = "hello";
+    public  $_db                 ;
+    public  $_db_name   = "lsapp";
     public  $_result;
-    public  $_fields=array();
-    public  $_field_list=array();
-    public  $_error   = "";
+    public  $_fields    = array();
+    public  $_where     = array();
+    public  $_field_list= array();
+    public  $_error     = "";
     
     public function __construct($table=''){
         
@@ -30,6 +32,11 @@ class Orm  {
     {
         $this->$name = $value;
     }
+    
+    public function __get($name){
+        return $name;
+    }
+    
     public function set($field,$value){
         $this->_fields[$field]['value'] = $value;
         $this->$field = $value;
@@ -67,7 +74,9 @@ class Orm  {
     }
     
     public function data(){
-        return $this->_result->fetch_all(MYSQLI_ASSOC);
+        $data = $this->_result->fetch_all(MYSQLI_ASSOC);
+        $this->_result->free();
+        return $data ;
     }
     
     public function last_sql(){
@@ -113,14 +122,19 @@ class Orm  {
         }else{
             $this->all();
         }
-        $this->query($this->_sql);
+        $this->query();
         return $this;
     }
     
     
     
-    public function find($id){
-        $data = $this->get($id)->row();
+    public function find($id=""){
+        
+        if($id){
+            $data = $this->get($id)->row();
+        }else{
+            $data = $this->all()->query()->row();
+        }
         
         if(!$data){
             $this->_error = "No data found";
@@ -133,9 +147,48 @@ class Orm  {
         return $this;
     }
     
-    public function query($sql='',$resultmode = MYSQLI_USE_RESULT){
+    public function findall(){
+       return $this->all()->query()->data();
+    }
+    
+    public function findall_object(){
+        $this->all()->query();
+        $all_data = array();
+        while($data = $this->_result->fetch_object()){
+            $all_data[] = $data;
+        }
+        $this->_result->free();
+        return $all_data;
+    }
+    
+    public function findall_orm(){
+        $this->all()->query();
+        $temp_orm = $this;
+        $all_data = array();
+        while($data = $this->_result->fetch_object()){
+            foreach($this->_field_list as $field){
+                $temp_orm->$field = $data->$field;
+            }
+            $all_data[] = $temp_orm;
+        }
+        //$this->_result->free();
+        return $all_data;
+    }
+    
+    public function query($sql='',$resultmode = MYSQLI_STORE_RESULT){
         if(!$sql)
             $sql = $this->_sql;
+        $where_sql = '';
+        if($this->_where){
+            foreach($this->_where as $key=>$value){
+                $where_sql .= " `$key`='$value' AND ";                
+            }
+            $where_sql  = " WHERE ".$where_sql."1";
+            $this->_where_sql = $where_sql;
+        }
+        
+        $sql .= $where_sql; 
+        
         $this->_result = $this->_db->query($sql,$resultmode);
         
         if(!$this->_result){
@@ -156,8 +209,8 @@ class Orm  {
                $this->_sql .= " $field='{$this->$field}',";
            }
            $this->_sql  = rtrim($this->_sql,',');
-           $this->_sql .= " where id='{$this->_id}'";
-           $this->query($this->_sql);
+           $this->where("id",$this->_id);
+           $this->query();
            return $this;
        }
        $this->id = '';
@@ -168,7 +221,7 @@ class Orm  {
        }
        $this->_sql  = rtrim($this->_sql,',');
        $this->_sql .= ")";
-       $this->query($this->_sql);
+       $this->query();
        $this->_id   = $this->insert_id();
        $this->id    = $this->_id;
        return $this; 
@@ -177,10 +230,20 @@ class Orm  {
     public function delete($id=''){
         if(!$id)
             $id = $this->_id;
-        $this->_sql = "Delete FROM {$this->_table} where id = '$id'";
+        $this->_sql = "Delete FROM {$this->_table} ";
+        $this->where("id",$id);
         $this->query();
         $this->id = '';
         $this->_id = '';
+        return $this;
+    }
+    
+    public function where($name,$value){
+        if(!is_array($name)){
+            $this->_where[$name] = $value;            
+        }else{
+            $this->_where = $name;
+        }   
         return $this;
     }
     
@@ -202,13 +265,10 @@ class Orm  {
 
 $orm = new Orm("posts");
 
-$orm->find(21);
-$orm->title     = "Farhad  ";
-$orm->body      = "this is body";
-$orm->user_id   = 1;
+$data = $orm->where("title", "Farhad")->findall_orm();
 
+print_r($data);
 
-$orm->save();
 
 
 
